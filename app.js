13df44ed6e01358c98394484085d9f1a82f46483
daimessdn <daimessdn@gmail.com>
2020-01-08 20:39:13 +0700

    attached mimes...

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
-const http = require('http');
-const fs = require('fs');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
 
+const mimetypes = {
+    'html': 'text/html',
+    'css': 'text/css',
+    'js': 'text/javascript',
+    'png': 'image/png',
+    'jpeg': 'image/jpeg',
+    'jpg': 'image/jpg'
+};
+
+
+var portname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((request, response) => {
-	const readStream = fs.createReadStream('./static/index.html');
+http.createServer((req, res) => {
+    var myuri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), unescape(myuri));
+    console.log('File you are looking for is:' + filename);
+    var loadFile;
 
-	response.writeHead(200, {'Content-type': 'text/html'});
-	readStream.pipe(response);
+    try {
+        loadFile = fs.lstatSync(filename);
+    } catch (error) {
+        res.writeHead(404, {
+            "Content-Type": 'text/plain'
+        });
+        res.write('404 Internal Error');
+        res.end();
+        return;
+    }
 
-	// response.write('Hello world from Nodejs!');
-	// response.end();
-});
+    if (loadFile.isFile()) {
+        var mimeType = mimetypes[path.extname(filename).split('.').reverse()[0]];
+        res.writeHead(200, {
+            "Content-Type": mimeType
+        });
+        var filestream = fs.createReadStream(filename);
+        filestream.pipe(res);
+    } else if (loadFile.isDirectory()) {
+        res.writeHead(302, {
+            'Location': 'static/index.html'
+        });
+        res.end();
+    } else {
+        res.writeHead(500, {
+            "Content-Type": 'text/plain'
+        });
+        res.write('500 Internal Error');
+        res.end();
+    }
 
-server.listen(port, () =>{
-	console.log('Server is running at port' + port + ' on your browser...');	
+}).listen(port, portname, () => {
+    console.log(`Server is running on server http://${portname}:${port}`);
 });
